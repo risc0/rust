835fd4ca4c63041e53ad9fba35c5de1dd36885eb
Victor Graf <victor@risczero.com>
2023-05-19 09:44:48 -0700

    add an initial env var based args impl

---
+++ library/std/src/sys/zkvm/args.rs
@@ -0,0 +1,52 @@
+use super::os;
+use crate::ffi::OsString;
+use crate::fmt;
+use crate::str::FromStr;
+
+pub struct Args {
+    i: usize,
+    count: usize,
+}
+
+pub fn args() -> Args {
+    let count = os::getenv(&OsString::from("ARGC"))
+        .map(|var| usize::from_str(var.into()).unwrap())
+        .unwrap_or(0);
+
+    Args { i: 0, count }
+}
+
+impl fmt::Debug for Args {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().finish()
+    }
+}
+
+impl Iterator for Args {
+    type Item = OsString;
+
+    fn next(&mut self) -> Option<OsString> {
+        if i >= self.count {
+            None
+        } else {
+            let mut varname = OsString::from("ARGV_");
+            varname.push(&self.i.to_string().into());
+            Some(os::getenv(&varname).unwrap())
+        }
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.count, Some(self.count))
+    }
+}
+
+impl ExactSizeIterator for Args {
+    fn len(&self) -> usize {
+        self.count
+    }
+}
+
+impl DoubleEndedIterator for Args {
+    fn next_back(&mut self) -> Option<OsString> {
+        panic!("unsupported");
+    }
+}
